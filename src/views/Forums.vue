82a3e2af286c164bad6d2c5a618c92523fa88920
annacfarris <103215164+annacfarris@users.noreply.github.com>
2022-05-10 13:17:31 -0400

    Add files via upload

---
+++ src/views/Forums.vue
@@ -0,0 +1,347 @@
+<script>
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+</script>
+
+<template>
+  <nav>
+    <div class="fixed-header">
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
+      <a href="/" class="brand-logo"
+        ><img id="logo" src="../assets/logo-image.png"
+      /></a>
+      <div class="nav-bar">
+        <a href="./Forums">Forum</a>
+        <a href="./Movies">Movies</a>
+        <a href="./Profile">Profile</a>
+      </div>
+      <form class="example" action="/action_page.php">
+        <input type="text" placeholder="Search this forum..." name="search" />
+        <button type="submit"><i class="fa fa-search"></i></button>
+      </form>
+
+      <div class="bbground">
+        <div class="boxcont">
+          <div class="container3">
+            <img src="../assets/avatar1.png" alt="Avatar" style="width: 90px" />
+            <p><span>ywnam</span> Oscars Slap</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+
+          <div class="container3">
+            <img src="../assets/avatar2.png" alt="Avatar" style="width: 90px" />
+            <p><span>txwom</span> Oscars Slap</p>
+            <p>Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus</p>
+          </div>
+
+          <div class="container3">
+            <img src="../assets/avatar4.png" alt="Avatar" style="width: 90px" />
+            <p><span>xwmao</span> Oscars Slap</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+
+        <div class="boxcont">
+          <div class="container3">
+            <img src="../assets/avatar1.png" alt="Avatar" style="width: 90px" />
+            <p><span>wvem</span> Morbius</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+
+          <div class="container3">
+            <img src="../assets/avatar2.png" alt="Avatar" style="width: 90px" />
+            <p><span>suemx</span> Morbius</p>
+            <p>Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus</p>
+          </div>
+
+          <div class="container3">
+            <img src="../assets/avatar4.png" alt="Avatar" style="width: 90px" />
+            <p><span>ogruv</span> Morbius</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+
+        <button class="open-button" onclick="openForm()">Post</button>
+
+        <div class="chat-popup" id="myForm">
+          <form action="/action_page.php" class="form-container">
+            <h1>Post</h1>
+
+            <label for="msg"><b>Message</b></label>
+            <textarea
+              placeholder="Type message.."
+              name="msg"
+              required
+            ></textarea>
+
+            <button type="submit" class="btn">Send</button>
+            <button type="button" class="btn cancel" onclick="closeForm()">
+              Close
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+@import "../assets/base.css";
+
+body {
+  background-color: #eeeadb;
+  font-weight: normal;
+  padding-top: 100px;
+  padding-bottom: 100px;
+  overflow: scroll;
+}
+.background {
+  background-color: #eeeadb;
+  padding-top: 100px;
+  padding-bottom: 100px;
+  height: 100px;
+}
+
+.fixed-header {
+  top: 0;
+}
+.fixed-footer {
+  top: 0;
+}
+.fixed-header,
+.fixed-footer {
+  width: 100%;
+  position: fixed;
+  background: white;
+  padding: 10px 0;
+  color: white;
+  z-index: 1;
+}
+nav a {
+  color: black;
+  text-decoration: none;
+  padding: 7px 25px;
+  display: inline-block;
+  font-size: 27px;
+  font-family: "Lucida Console", "Courier New", monospace;
+}
+
+.nav-bar {
+  float: right;
+}
+
+#logo {
+  width: 100%;
+  height: 40px;
+}
+
+body {
+  font-family: Arial;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.bbground {
+  background-color: #eeeadb;
+  padding-top: 70px;
+  padding-bottom: 100px;
+}
+
+form.example input[type="text"] {
+  padding: 10px;
+  font-size: 17px;
+  border: 1px solid grey;
+  float: left;
+  width: 80%;
+  background: whitesmoke;
+}
+
+form.example button {
+  float: left;
+  width: 20%;
+  padding: 10px;
+  background: #195a68;
+  color: white;
+  font-size: 17px;
+  border: 1px solid grey;
+  border-left: none;
+  cursor: pointer;
+}
+
+form.example button:hover {
+  background: #195a68;
+}
+
+form.example::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.pagination {
+  display: inline-block;
+}
+
+.pagination a {
+  color: black;
+  float: left;
+  padding: 1px 16px;
+  text-decoration: none;
+  border: 1px solid #ddd;
+}
+
+.pagination a.active {
+  background-color: #195a68;
+  color: white;
+  border: 1px solid #0b7dda;
+}
+
+.pagination a:hover:not(.active) {
+  background-color: #ddd;
+}
+
+.pagination a:first-child {
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+}
+
+.pagination a:last-child {
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+}
+
+.boxcont {
+  background-color: #d1c0a8;
+  
+  padding: 1px;
+  margin: 20px;
+  border-radius: 15px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  max-width: 100%;
+}
+
+.container3 {
+  border: 2px solid #ccc;
+  background-color: #195a68;
+  border-radius: 15px;
+  padding: 16px;
+  margin: 16px 0;
+}
+
+.container3::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.container3 img {
+  float: left;
+  margin-right: 20px;
+  border-radius: 50%;
+}
+
+.container3 span {
+  font-size: 20px;
+  margin-right: 15px;
+}
+
+@media (max-width: 500px) {
+  .container3 {
+    text-align: center;
+  }
+  .container3 img {
+    margin: auto;
+    float: none;
+    display: block;
+  }
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  box-sizing: border-box;
+}
+
+/* Button used to open the chat form - fixed at the bottom of the page */
+.open-button {
+  background-color: #555;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  opacity: 0.8;
+  position: fixed;
+  bottom: 23px;
+  right: 28px;
+  width: 280px;
+}
+
+/* The popup chat - hidden by default */
+.chat-popup {
+  display: none;
+  position: fixed;
+  bottom: 0;
+  right: 15px;
+  border: 3px solid #f1f1f1;
+  z-index: 9;
+}
+
+/* Add styles to the form container */
+.form-container {
+  max-width: 300px;
+  padding: 10px;
+  background-color: white;
+}
+
+/* Full-width textarea */
+.form-container textarea {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  border: none;
+  background: #f1f1f1;
+  resize: none;
+  min-height: 200px;
+}
+
+/* When the textarea gets focus, do something */
+.form-container textarea:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Set a style for the submit/send button */
+.form-container .btn {
+  background-color: #04aa6d;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  margin-bottom: 10px;
+  opacity: 0.8;
+}
+
+/* Add a red background color to the cancel button */
+.form-container .cancel {
+  background-color: red;
+}
+
+/* Add some hover effects to buttons */
+.form-container .btn:hover,
+.open-button:hover {
+  opacity: 1;
+}
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,384 @@
+<script>
+import Editor from '../components/Editor.vue'
+
+export default {
+  components: {
+    Editor,
+  },
+
+  data() {
+    return {
+      content: '<h1>Jane Doe</h1>',
+      content2: '<p>Edit Bio Here</p>'
+    }
+    
+  },
+}
+</script>
+
+<template>
+  <nav>
+    <div class="fixed-header">
+      <a href="/" class="brand-logo"
+        ><img id="logo" src="../assets/logo-image.png"
+      /></a>
+      <div class="nav-bar">
+        <a href="./Forums">Forum</a>
+        <a href="./Movies">Movies</a>
+        <a href="./Profile">Profile</a>
+      </div>
+    </div>
+  </nav>
+
+  <main>
+    <br /><br />
+    <div class="favorites">
+      <h2 id="movieText">Favorite Movies</h2>
+      <div class="imgContainer">
+        <img
+          class="fave-image"
+          src="../assets/harrypotter.png"
+          alt="Harry Potter Movie Cover"
+        />
+      </div>
+      <div class="imgContainer">
+        <img
+          class="fave-image"
+          src="../assets/hunger games.png"
+          alt="Hunger Games Movie Cover"
+        />
+      </div>
+      <div class="imgContainer">
+        <img
+          class="fave-image"
+          src="../assets/spiderman.png"
+          alt="Spiderman Movie Cover"
+        />
+      </div>
+    </div>
+
+    <div class="userbackground">
+      <div class="user">
+        <img id="profileimg" src="../assets/profilepicavatar.png" />
+      </div>
+      <div class="name">
+        <editor v-model="content" />
+      </div>
+      <div class="bio">
+        <editor v-model="content2" />
+      </div>
+      
+    </div>
+    
+    <div class="friends">
+      <h2 id="friendText">Friends List</h2>
+      <button class="btn seeMore">See More</button>
+      <div class="row">
+        <div class="column">
+          <img class ="userimg" src="../assets/avatar1.png" />
+          <img class ="userimg" src="../assets/avatar2.png" />
+          <img class ="userimg" src="../assets/avatar3.png" />
+          <img class ="userimg" src="../assets/avatar4.png" />
+          <img class ="userimg" src="../assets/avatar5.png" />
+          <img class ="userimg" src="../assets/avatar6.png" />
+          <img class ="userimg" src="../assets/avatar3.png" />
+          <img class ="userimg" src="../assets/avatar1.png" />
+          <img class ="userimg" src="../assets/avatar9.png" />
+          <img class ="userimg" src="../assets/avatar8.png" />
+          <img class ="userimg" src="../assets/avatar6.png" />
+          <img class ="userimg" src="../assets/avatar7.png" />
+        </div>
+      </div>
+    </div>
+    <div class="forum">
+      <button class="forumBtn" onclick="location.href='./Forums'">Go to Forum</button>
+      <h1 class="head">Jane's Recent Responses</h1>
+      <div class="container">
+        <div class="containertxt">
+          <img src="../assets/profilepicavatar.png" alt="Avatar" >
+          <p><span>xwmao</span> Oscars Slap</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+      </div>
+
+
+      <div class="container">
+        <div class="containertxt">
+          <img src="../assets/profilepicavatar.png" alt="Avatar" />
+          <p><span>xwmao</span> Oscars Slap</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+      </div>
+      
+      <div class="container">
+        <div class="containertxt">
+          <img src="../assets/profilepicavatar.png" alt="Avatar" />
+          <p><span>xwmao</span> Oscars Slap</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+@import "../assets/base.css";
+
+body {
+  background-color: #eeeadb;
+  font-weight: normal;
+  padding-top: 60px;
+  padding-bottom: 40px;
+}
+nav a:hover{
+  color: grey;
+}
+
+.fixed-header {
+  top:0;
+}
+.fixed-footer {
+  top:0;
+}
+.fixed-header, .fixed-footer{
+        width: 100%;
+        position: fixed;        
+        background: white;
+        padding: 10px 0;
+        color: white;
+        z-index: 2;
+}
+nav a{
+        color: black;
+        text-decoration: none;
+        padding: 7px 25px;
+        display: inline-block;
+        font-size: 27px;
+        font-family: "Lucida Console", "Courier New", monospace;
+      
+    }
+
+.nav-bar {
+float:right;
+}
+
+#logo {
+   width: 100%;
+  height: 40px;
+}
+.imgContainer {
+  float: left;
+  padding: 5px;
+
+}
+.favorites {
+  padding: 15px;
+  background-color: #e8ded1;
+  background-clip: padding-box;
+  width: 800px;
+  height: 500px;
+  padding-top: 30px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  float:left;
+  border-radius: 25px;
+  
+}
+
+
+.fave-image {
+  width:230px;
+  height: auto;
+  cursor: pointer;
+}
+#movieText {
+  padding-bottom: 20px;
+  font-size: 27px;
+  font-family: "Lucida Console", "Courier New", monospace;
+}
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0 4px;
+}
+
+/* Create two equal columns that sits next to each other */
+.column {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 75%;
+  padding: 10px;
+}
+
+.column img {
+  margin-top: 8px;
+  margin-right: 2px;
+  margin-left: 2px;
+  vertical-align: middle;
+  float:right;
+  height:78px;
+  
+}
+
+.friends {
+  position:absolute;left:1019px; top:500px; 
+  
+  background: white;
+  background-clip: padding-box;
+  width: 500px;
+  height:auto;
+  padding-top: 30px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  max-height:350px;
+  overflow-y:scroll;
+  border-radius: 25px;
+}
+#friendText{
+  text-align: right;
+  padding-right: 10px;
+  font-size: 27px;
+  font-family: "Lucida Console", "Courier New", monospace;
+
+}
+.seeMore {
+  float:right;
+  padding-right:10px;
+  color: blue;
+
+}
+.btn1 {
+  border: none;
+  font-size: 14px;
+  cursor: pointer;
+  display: inline-block;
+  text-decoration: underline;
+  
+
+}
+.forumBtn:hover {
+   color: grey;
+}
+
+/*.user {
+  float: left;
+  background-color: white;
+  height: 300px;
+  width:300px;
+  padding: 35px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  
+  
+}*/
+#profileimg {
+  float:left;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 50%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  z-index: 1;
+}
+
+.userbackground {
+  float: right;
+  background-color: #d1c0a8;
+  padding:13px;
+  height:325px;
+  width: 500px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 25px;
+}
+.bio, .name {
+  font-family: "Lucida Console", "Courier New", monospace;
+  text-align: center;
+  background-color: #e8ded1;
+  border-radius: 25px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+}
+
+
+.forum {
+  background-color: #d1c0a8;
+  position:absolute;left:0px; top:650px; 
+  padding:20px;
+  padding-top:30px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 25px;
+  
+}
+.container {
+  border: 2px solid #ccc;
+  background-color: #eee;
+  border-radius: 5px;
+  margin: 16px 0;
+  height: 100px;
+  width:900px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  margin-top: 25px
+  
+}
+.forum h1 {
+  font-family: "Lucida Console", "Courier New", monospace;
+}
+.container span {
+  font-size: 12px;
+  margin-right: 15px;
+
+  
+}
+
+.containertxt{
+  text-align:left;
+  float: left;
+}
+.container img {
+  float: left;
+  margin-right: 20px;
+  border-radius: 50%;
+  height:80px;
+  margin-top:10px;
+
+}
+.btn .forumBtn {
+  float:right;
+  color: #00008B;
+  background-color: transparent;
+  
+}
+
+.ProseMirror {
+  > * + * {
+    margin-top: 0.75em;
+  }
+
+  code {
+    background-color: rgba(#616161, 0.1);
+    color: #616161;
+  }
+}
+
+.content {
+  padding: 1rem 0 0;
+
+  h3 {
+    margin: 1rem 0 0.5rem;
+  }
+
+  pre {
+    border-radius: 5px;
+    color: #333;
+  }
+
+  code {
+    display: block;
+    white-space: pre-wrap;
+    font-size: 0.8rem;
+    padding: 0.75rem 1rem;
+    background-color:#e9ecef;
+    color: #495057;
+  }
+
+}
+
+</style>
